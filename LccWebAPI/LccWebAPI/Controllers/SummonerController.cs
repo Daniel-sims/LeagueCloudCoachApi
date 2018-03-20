dbfad67a288ae7ac2acbed4a77bccf7c421c6cf5
Daniel Sims <daniel.sims1994@outlook.com>
2018-03-20 19:07:25 +0000

    summoner controller

---
+++ LccWebAPI/LccWebAPI/Controllers/SummonerController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RiotSharp.Interfaces;
+
+namespace LccWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class SummonerController : Controller
+    {
+        private readonly IRiotApi _riotApi;
+
+        public SummonerController(IRiotApi riotApi)
+        {
+            _riotApi = riotApi;
+        }
+
+        [HttpGet("Summoner/{summonerName}")]
+        public async Task<JsonResult> GetSummonerByName(string summonerName)
+        {
+            var summoner = await _riotApi.GetSummonerByNameAsync(RiotSharp.Misc.Region.euw, summonerName);
+
+            return new JsonResult(summoner);
+        }
+
+
+    }
+}
